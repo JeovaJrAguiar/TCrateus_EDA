85dd2c6383e0dc0a06390c7ba6950ee6272a653c
EriclesC <51061062+EriclesC@users.noreply.github.com>
2022-04-30 10:45:59 -0300

    Cadastro de setores parcialmente concluído

--- main.c
+++ main.c
@@ -1,10 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lib.h"
 
-
-
-int main(int argc, char *argv[]) {
-	printf("teste");
-	return 0;
+int main(void) {
+	int op;
+	TLista L;
+	Tdado dado;
+	criar(&L);
+	int hash;
+	do{    
+    //Exibir menu
+        system("cls");
+        puts("\n\t\t\t\tEDITOR DE LISTAS\n");
+        puts("\t1  - EXIBIR LISTA\n \t2  - INSERIR UM SETOR\n \t0  - SAIR");
+        printf("\nINFORME SUA OPCAO:\n");
+        scanf("%d", &op);//Escolha da Opção
+        
+    	switch(op)
+        	{
+        		case 1:{
+        			exibir(L);
+					break;
+				}
+				
+				case 2:{
+					printf("Informe uma chave: ");
+	                scanf("%d", &dado.chave);
+	                
+	                printf("Informe o nome: ");
+	                scanf("%s", &dado.nome);
+	                
+	                hash = hashing(dado.chave);
+	                inserir(&L,hash, dado);
+					break;
+				}
+        	// Outra opção nao oferecida
+    		default:
+    			puts("OPCAO INCORRETA,TENTAR NOVAMENTE.");break;
+      
+        }
+	getch();
+    }while(op!=0);
+  
+  system("PAUSE");	
+  return 0;
 }
--- modelo.c
+++ modelo.c
@@ -1,1 +1,49 @@
 #include "lib.h"
+
+int criar(TLista *L){
+	int i=0;
+	for(i=0;i<MAX;i++)
+		L->dado[i]= NULL;
+	L->n = 0;
+	return 1;	
+}
+
+int inserir(TLista *L, int hash, Tdado dado){
+	Tno novo;
+	novo = (Tno*) malloc(sizeof(Tno));
+	novo->dado = dado;
+	if(L->dado[hash]==NULL){
+		L->dado[hash] = novo;
+		novo->proximo = NULL;
+	}
+	else{
+		novo->proximo = L->dado[hash];
+		L->dado[hash] = novo;
+	}
+	L->n+=1;
+	return 1;
+}
+
+void exibir(TLista L){
+	int i;
+	Tno aux;
+	if(L.n!=0){
+		for(i=0;i<MAX;i++){
+			if(L.dado[i] != NULL){
+				printf("---------indice %d---------\n", i);
+				 aux = 	L.dado[i];
+				 while(aux !=NULL){
+				 	printf("Chave = %d\n", aux->dado.chave);
+				 	printf("Nome = %s\n\n", aux->dado.nome);
+				 	aux = aux->proximo;
+				 }
+			}
+		}
+	}
+	else
+		printf("Tabela Vazia");
+}
+
+int hashing(int chave){
+	return (chave % MAX);
+}
